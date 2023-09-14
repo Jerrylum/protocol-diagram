9e01926d6d845b086c43c44172b0a083e3f9e576
Jerrylum <serverofjerry@gmail.com>
2023-09-14 17:40:06 +0800

    PD-20 Add BottomPanel and export buttons

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,13 +2,13 @@ import "./Root.scss";
 import { observer } from "mobx-react-lite";
 import { DiagramCanvas } from "./app/DiagramCanvas";
 import { Box } from "@mui/material";
-import { CommandInputField } from "./app/CommandInputField";
+import { BottomPanel } from "./app/BottomPanel";
 
 const Root = observer(() => {
   return (
     <Box id="root-container">
       <DiagramCanvas />
-      <CommandInputField />
+      <BottomPanel />
     </Box>
   );
 });
