aa1a426efd5ce10e7035267706ffd8447fbcec4a
Jerrylum <serverofjerry@gmail.com>
2023-09-15 19:50:55 +0800

    PD-39 Remove some test code

--- src/app/DiagramCanvas.tsx
+++ src/app/DiagramCanvas.tsx
@@ -7,8 +7,6 @@ import { getWindowSize } from "../core/Util";
 import { useBetterMemo } from "../core/Hook";
 import React from "react";
 import Konva from "konva";
-// import TestMarkdown from "./TestMarkdown.mdx";
-// import { MarkdownOverwrittenComponents } from "./MarkdownComponents";
 
 export class DiagramCanvasController {
   canvasSize: Vector = getWindowSize();
@@ -38,7 +36,6 @@ export const DiagramCanvas = observer(() => {
         onContextMenu={e => e.evt.preventDefault()}>
         <Layer></Layer>
       </Stage>
-      {/* <TestMarkdown components={MarkdownOverwrittenComponents} /> */}
     </Box>
   );
 });
