656830ffffe0e0278fd0d6806c1a14718252c38f
Jerrylum <serverofjerry@gmail.com>
2023-09-15 19:32:01 +0800

    PD-39 Implement Modals

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,14 +3,22 @@ import { observer } from "mobx-react-lite";
 import { DiagramCanvas } from "./app/DiagramCanvas";
 import { Box } from "@mui/material";
 import { BottomPanel } from "./app/BottomPanel";
+// import { TestModal, TestModalSymbol } from "./app/TestModal";
+// import React from "react";
+// import { getRootStore } from "./core/Root";
 
 const Root = observer(() => {
+  // React.useEffect(() => {
+  //   getRootStore().modals.open(TestModalSymbol);
+  // }, []);
   return (
     <Box id="root-container">
       <DiagramCanvas />
       <BottomPanel />
+      {/* <TestModal /> */}
     </Box>
   );
 });
 
 export default Root;
+
