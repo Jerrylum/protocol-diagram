f49aaf89ae30c74651f3d30f19939ca9f109703f
crapper <crapper.612@gmail.com>
2023-09-16 21:40:38 +0800

    PD-50 Add EnumOption, EnumOption Test

--- src/config/Option.test.ts
+++ src/config/Option.test.ts
@@ -1,5 +1,5 @@
 import { CodePointBuffer, Parameter } from "../token/Tokens";
-import { BooleanOption } from "./Option";
+import { BooleanOption, EnumOption } from "./Option";
 
 test('BooleanOption Get', () => {
     const option = new BooleanOption("test", true);
@@ -51,4 +51,44 @@ test('BooleanOption Set Parameter', () => {
     const result5 = option.setValue(Parameter.parse(new CodePointBuffer("0"))!);
     expect(result5.success).toBe(false);
     expect(result5.message).toBe("The value must be a boolean.");
+});
+
+test('EnumOption Get', () => {
+    const option = new EnumOption("test", "a", ["a", "b", "c"]);
+    expect(option.key).toBe("test");
+    expect(option.defaultValue).toBe("a");
+    expect(option.getValue()).toBe("a");
+    expect(option.getUsageDescription()).toBe("A | b | c");
+});
+
+test('EnumOption Set String', () => {
+    const option = new EnumOption("test", "aaa", ["aaa", "bbb", "ccc", "abc"]);
+    expect(option.setValue("bbb").success).toBe(true);
+    expect(option.getValue()).toBe("bbb");
+    expect(option.defaultValue).toBe("aaa");
+    expect(option.setValue("bbb").success).toBe(false);
+    expect(option.setValue("bbb").message).toBe("It is already \"bbb\".");
+    expect(option.setValue("a").success).toBe(false);
+    expect(option.setValue("a").message).toBe("Ambiguous value \"a\".");
+    expect(option.setValue("ddd").success).toBe(false);
+    expect(option.setValue("ddd").message).toBe("Unknown value \"ddd\".");
+    expect(option.setValue("abc").message).toBe("Set \"test\" from \"bbb\" to \"abc\".");
+    expect(option.setValue("bb").message).toBe("Set \"test\" from \"abc\" to \"bbb\".");
+});
+
+test('EnumOption Set Parameter', () => {
+    const option = new EnumOption("test", "aaa", ["aaa", "bbb", "ccc", "abc"]);
+    expect(option.setValue(Parameter.parse(new CodePointBuffer("bbb"))!).success).toBe(true);
+    expect(option.getValue()).toBe("bbb");
+    expect(option.defaultValue).toBe("aaa");
+    expect(option.setValue(Parameter.parse(new CodePointBuffer("bbb"))!).success).toBe(false);
+    expect(option.setValue(Parameter.parse(new CodePointBuffer("bbb"))!).message).toBe("It is already \"bbb\".");
+    expect(option.setValue(Parameter.parse(new CodePointBuffer("a"))!).success).toBe(false);
+    expect(option.setValue(Parameter.parse(new CodePointBuffer("a"))!).message).toBe("Ambiguous value \"a\".");
+    expect(option.setValue(Parameter.parse(new CodePointBuffer("ddd"))!).success).toBe(false);
+    expect(option.setValue(Parameter.parse(new CodePointBuffer("ddd"))!).message).toBe("Unknown value \"ddd\".");
+    expect(option.setValue(Parameter.parse(new CodePointBuffer("abc"))!).message).toBe("Set \"test\" from \"bbb\" to \"abc\".");
+    expect(option.setValue(Parameter.parse(new CodePointBuffer("bb"))!).message).toBe("Set \"test\" from \"abc\" to \"bbb\".");
+    expect(option.setValue(Parameter.parse(new CodePointBuffer("5"))!).success).toBe(false);
+    expect(option.setValue(Parameter.parse(new CodePointBuffer("5"))!).message).toBe("The value \"5\" is not accepted.");
 });
--- src/config/Option.ts
+++ src/config/Option.ts
@@ -1,4 +1,3 @@
-import { boolean } from "yargs";
 import { HandleResult, success, fail } from "../command/HandleResult";
 import { Parameter } from "../token/Tokens";
 
@@ -8,104 +7,185 @@ import { Parameter } from "../token/Tokens";
  * changed afterward
  */
 export abstract class Option<T> {
+  /**
+   * the key of this option, it should be treated as a readonly value,
+   * since by logic, the key of a option should not be able to be changed,
+   * otherwise it would cause confusions
+   */
 
-    /**
-     * the key of this option, it should be treated as a readonly value,
-     * since by logic, the key of a option should not be able to be changed,
-     * otherwise it would cause confusions
-     */
+  constructor(readonly key: string) {}
 
-    constructor(readonly key: string) {}
+  /**
+   * a setter method that sets the value of this option
+   *
+   * @param value the value of this option
+   * @return HandleResult
+   */
+  public abstract setValue(value: Parameter | T): HandleResult;
 
-    /**
-     * a setter method that sets the value of this option
-     * 
-     * @param value the value of this option
-     * @return HandleResult
-     */
-    public abstract setValue(value: Parameter | T): HandleResult;
+  /**
+   * a getter method that retrieves the value of this option
+   *
+   * @return the value of this option
+   */
+  public abstract getValue(): T;
 
-    /**
-     * a getter method that retrieves the value of this option
-     * 
-     * @return the value of this option
-     */
-    public abstract getValue(): T;
+  /**
+   * a getter method that retrieves the default value of this option
+   *
+   * @return the default value of this option
+   */
+  public abstract getDefault(): T;
 
-    /**
-     * a getter method that retrieves the default value of this option
-     * 
-     * @return the default value of this option
-     */
-    public abstract getDefault(): T;
-
-    /**
-     * a method that returns a manual statement for displaying the usage description
-     * 
-     * @return the usage description of this option
-     */
-    public abstract getUsageDescription(): string;
-
+  /**
+   * a method that returns a manual statement for displaying the usage description
+   *
+   * @return the usage description of this option
+   */
+  public abstract getUsageDescription(): string;
 }
 
 export class BooleanOption extends Option<boolean> {
+  private value: boolean;
 
-    private value: boolean;
+  public constructor(key: string, readonly defaultValue: boolean) {
+    super(key);
+    this.value = defaultValue;
+  }
 
-    public constructor(key: string, readonly defaultValue:boolean) {
-        super(key);
-        this.value = defaultValue;
+  /**
+   * a wrapper method that sets the value of this boolean option, the value of the
+   * parameter is required to be an boolean
+   *
+   * @return whether the value is set successfully
+   */
+  setValue(value: Parameter | boolean): HandleResult {
+    if (value instanceof Parameter) {
+      if (value.isBoolean()) {
+        return this.setValue(value.getBoolean());
+      } else {
+        return fail("The value must be a boolean.");
+      }
+    } else {
+      const oldValue = this.value;
+      if (oldValue === value) {
+        return fail("It is already " + value + ".");
+      } else {
+        this.value = value;
+        return success('Set "' + this.key + '" from ' + oldValue + " to " + value + ".");
+      }
     }
+  }
 
-    /**
-     * a wrapper method that sets the value of this boolean option, the value of the
-     * parameter is required to be an boolean
-     * 
-     * @return whether the value is set successfully
-     */
-    setValue(value: Parameter | boolean): HandleResult {
-        if (value instanceof Parameter){
-            if (value.isBoolean()) {
-                return this.setValue(value.getBoolean());
-            } else {
-                return fail("The value must be a boolean.");
-            }
-        }else {
-          const oldValue = this.value;
-          if (oldValue === value) {
-              return fail("It is already " + value + ".");
-          } else {
-              this.value = value;
-              return success("Set \"" + this.key + "\" from " + oldValue + " to " + value + ".");
-          }
+  /**
+   * a getter method that reads the value of this boolean option and returns it
+   *
+   * @return the value of this boolean option
+   */
+  getValue(): boolean {
+    return this.value;
+  }
+
+  /**
+   * a getter method that returns the default value of this boolean option
+   *
+   * @return the default value of this boolean option
+   */
+  getDefault(): boolean {
+    return this.defaultValue;
+  }
+
+  /**
+   * a method that returns a manual statement
+   *
+   * @return the usage description of this option
+   */
+  public getUsageDescription(): string {
+    return this.defaultValue ? "TRUE | false" : "true | FALSE";
+  }
+}
+
+/**
+ * this class is an option class that extends the logic of confining the
+ * possible values of string literals
+ */
+export class EnumOption extends Option<string> {
+  private value: string;
+
+  constructor(key: string, readonly defaultValue: string, private acceptedValues: string[]) {
+    super(key);
+    this.value = defaultValue;
+  }
+
+  /**
+   * a wrapper method that sets the value of the parameters, the value of the
+   * parameter is required to be string
+   *
+   * @param value the value of this option
+   * @return whether the value is set successfully
+   */
+  public setValue(value: Parameter | string): HandleResult {
+    if (value instanceof Parameter) {
+      if (value.isString()) {
+        return this.setValue(value.getString());
+      } else {
+        return fail('The value "' + value + '" is not accepted.');
+      }
+    } else {
+      let selected: string | null = null;
+      for (const acceptedValue of this.acceptedValues) {
+        if (acceptedValue === value.toLowerCase()) {
+          selected = acceptedValue;
+          break;
         }
-    }
+        if (acceptedValue.startsWith(value.toLowerCase())) {
+          if (selected != null) {
+            return fail('Ambiguous value "' + value + '".');
+          }
+          selected = acceptedValue;
+        }
+      }
 
-
-    /**
-     * a getter method that reads the value of this boolean option and returns it
-     * 
-     * @return the value of this boolean option
-     */
-    getValue(): boolean {
-        return this.value;
+      if (selected === null) {
+        return fail('Unknown value "' + value + '".');
+      } else {
+        let oldValue: string = this.value;
+        if (oldValue === selected) {
+          return fail('It is already "' + selected + '".');
+        } else {
+          this.value = selected;
+          return success('Set "' + this.key + '" from "' + oldValue + '" to "' + selected + '".');
+        }
+      }
     }
+  }
 
-    /**
-     * a getter method that returns the default value of this boolean option
-     * 
-     * @return the default value of this boolean option
-     */
-    getDefault(): boolean {
-        return this.defaultValue;
-    }
+  /**
+   * a getter method that retrieves the value of this enum option
+   *
+   * @return the value of this enum option
+   */
+  getValue(): string {
+    return this.value;
+  }
 
-    /**
-     * a method that returns a manual statement
-     * 
-     * @return the usage description of this option
-     */
-    public getUsageDescription(): string {
-        return this.defaultValue ? "TRUE | false" : "true | FALSE";
-    }
+  /**
+   * a getter method that retrieves the default value of this enum option
+   *
+   * @return the default value of this enum option
+   */
+  getDefault(): string {
+    return this.defaultValue;
+  }
+
+  /**
+   * a method that retrieves a manual statement for this enum option
+   *
+   * @return the manual statement for this enum option
+   */
+  getUsageDescription(): string {
+    return this.acceptedValues
+      .map(v => (v === this.defaultValue ? v.toUpperCase() : v))
+      .join(" | ");
+  }
 }
