46568c6c17d5a64d5e5de1ebd0646e76d0f2b3c9
crapper <crapper.612@gmail.com>
2023-09-16 16:12:41 +0800

    PD-56 Add KeyboardEvent to Command Input Field

--- src/app/CommandInputField.tsx
+++ src/app/CommandInputField.tsx
@@ -1,7 +1,18 @@
 import { Box, TextField } from "@mui/material";
 import { observer } from "mobx-react-lite";
+import { CommandLine, CodePointBuffer } from "../token/Tokens";
 
 export const CommandInputField = observer(() => {
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      const input = e.target as HTMLInputElement;
+      const command = input.value;
+      let cl = CommandLine.parse(new CodePointBuffer(command));
+      console.log(cl);
+      input.value = "";
+    }
+  };
+
   return (
     <Box
       sx={{
@@ -12,7 +23,7 @@ export const CommandInputField = observer(() => {
         left: "50%",
         transform: "translate(-50%, 0)"
       }}>
-      <TextField fullWidth size="small" />
+      <TextField fullWidth size="small" onKeyDown={handleKeyDown}/>
     </Box>
   );
 });
