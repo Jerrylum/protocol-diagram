d66be91ed06dd3a97e38df4ab5def9a44e565d06
crapper <crapper.612@gmail.com>
2023-09-21 16:00:39 +0800

    PD-50 Add Diagram to RootStore

--- src/core/Root.ts
+++ src/core/Root.ts
@@ -1,12 +1,14 @@
+import { Diagram } from "../diagram/Diagram";
+// import { MainApp } from "./MainApp"; // To be continued...
 import { Modals } from "./Modals";
 
 export type RootStore = typeof rootStore;
 
 const rootStore = {
+  diagram: new Diagram(),
   modals: new Modals()
 } as const;
 
 export function getRootStore(): RootStore {
   return rootStore;
 }
-
