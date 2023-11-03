a8f044d41cf1445827bc9636dd59ba748e149d98
Jerrylum <serverofjerry@gmail.com>
2023-11-03 16:04:52 +0800

    PD-95 Add hotkey symbols in the menu list

--- src/app/MainMenu.tsx
+++ src/app/MainMenu.tsx
@@ -1,9 +1,58 @@
-import { Box, Button, Divider, Menu, MenuItem } from "@mui/material";
+import { Box, Button, Divider, ListItemText, Menu, MenuItem, Typography } from "@mui/material";
 import { observer } from "mobx-react-lite";
 import MenuIcon from "@mui/icons-material/Menu";
 import React from "react";
 import { onDownload, onDownloadAs, onNew, onOpen, onSave, onSaveAs } from "../core/InputOutput";
 import { action } from "mobx";
+import { isMacOS } from "../core/Util";
+
+export const HotkeyTypography = observer((props: { hotkey: string | undefined }) => {
+  const { hotkey } = props;
+
+  if (hotkey === undefined) return null;
+
+  if (isMacOS(navigator.userAgent) === false)
+    return <Typography variant="body2" color="text.secondary" children={hotkey.replaceAll("Mod", "Ctrl")} />;
+
+  const temp = hotkey
+    .replaceAll("Mod", "⌘")
+    .replaceAll("Option", "⌥")
+    .replaceAll("Ctrl", "⌃")
+    .replaceAll("Shift", "⇧")
+    .replaceAll("CapsLock", "⇪")
+    .replaceAll("ArrowLeft", "←")
+    .replaceAll("ArrowRight", "→")
+    .replaceAll("ArrowUp", "↑")
+    .replaceAll("ArrowDown", "↓")
+    .replaceAll("Tab", "⇥")
+    .replaceAll("Del", "⌫")
+    .replaceAll(" ", "␣")
+    .replaceAll("Esc", "") // Hide escape key
+    .replaceAll("+", "")
+    .replaceAll("Add", "+")
+    .replaceAll("Equal", "+")
+    .replaceAll("Subtract", "-")
+    .replaceAll("Minus", "-");
+
+  const elements: React.ReactElement[] = [];
+  temp.split("").forEach((char, index) => {
+    elements.push(
+      <Typography
+        key={index}
+        variant="body2"
+        color="text.secondary"
+        sx={{
+          width: "1em",
+          textAlign: "center",
+          fontFamily: '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif'
+        }}
+        children={char}
+      />
+    );
+  });
+
+  return <>{elements}</>;
+});
 
 export const MainMenu = observer(() => {
   const menuBtnRef = React.useRef<HTMLButtonElement>(null);
@@ -49,25 +98,31 @@ export const MainMenu = observer(() => {
         anchorOrigin={{ vertical: "top", horizontal: "right" }}
         transformOrigin={{ vertical: "top", horizontal: "left" }}>
         <MenuItem dense onClick={onMenuButtonClick(onNew)}>
-          New
+          <ListItemText sx={{ marginRight: "1rem" }}>New</ListItemText>
+          <HotkeyTypography hotkey="Mod+P" />
         </MenuItem>
         <Divider />
         <MenuItem dense onClick={onMenuButtonClick(onOpen)}>
-          Open File
+          <ListItemText sx={{ marginRight: "1rem" }}>Open File</ListItemText>
+          <HotkeyTypography hotkey="Mod+O" />
         </MenuItem>
         <Divider />
         <MenuItem dense onClick={onMenuButtonClick(onSave)}>
-          Save
+          <ListItemText sx={{ marginRight: "1rem" }}>Save</ListItemText>
+          <HotkeyTypography hotkey="Mod+S" />
         </MenuItem>
         <MenuItem dense onClick={onMenuButtonClick(onSaveAs)}>
-          Save As
+          <ListItemText sx={{ marginRight: "1rem" }}>Save As</ListItemText>
+          <HotkeyTypography hotkey="Shift+Mod+S" />
         </MenuItem>
         <Divider />
         <MenuItem dense onClick={onMenuButtonClick(onDownload)}>
-          Download
+          <ListItemText sx={{ marginRight: "1rem" }}>Download</ListItemText>
+          <HotkeyTypography hotkey="Mod+D" />
         </MenuItem>
         <MenuItem dense onClick={onMenuButtonClick(onDownloadAs)}>
-          Download As
+          <ListItemText sx={{ marginRight: "1rem" }}>Download As</ListItemText>
+          <HotkeyTypography hotkey="Shift+Mod+D" />
         </MenuItem>
       </Menu>
     </>
